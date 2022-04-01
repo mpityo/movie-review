0bf9f14786251fc897c53159cae688a8af48e705
Matt Pityo <mpityo@gmail.com>
2022-04-01 18:07:29 -0400

    start api call and init server/connection

---
+++ util/api-call.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const baseURL = 'https://api.themoviedb.org/';
+const apiKey = process.env.MOVIEDB_API_KEY;
+
+// recent
+// popular
+// now showing
+async function getNowShowingAPI () {
+    
+}
+// genre
+
+async function getMovieDetail () {
+    const response = await fetch()
+}
+
+module.exports = getMovieDetail;
