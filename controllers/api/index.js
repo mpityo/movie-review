3aa1c44be9f1f3ad0e39fd3ebb139bf1cc49ce4c
Matt Pityo <mpityo@gmail.com>
2022-04-05 17:16:42 -0400

    add user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const movieRoutes = require('./movie-routes');
 const reviewRoutes = require('./review-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/movies', movieRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
